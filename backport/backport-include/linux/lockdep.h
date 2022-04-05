nbd168/backports

commit af58b27b1b1ab1133c0fec4d817cd6360ebe280e

headers: Add lockdep_assert_not_held()
The lockdep_assert_not_held() define was added in kernel 5.13 and is
used by ath10k now.
The code was copied from the upstream kernel.

Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

backport/backport-include/linux/lockdep.h
@@ -9,4 +9,15 @@ struct lockdep_map { };
 #endif /* CONFIG_LOCKDEP */
 #endif /* LINUX_VERSION_IS_LESS(4,15,0) */
 
+#ifndef lockdep_assert_not_held
+#ifdef CONFIG_LOCKDEP
+#define lockdep_assert_not_held(l)	do {				\
+		WARN_ON(debug_locks &&					\
+			lockdep_is_held(l) == LOCK_STATE_HELD);		\
+	} while (0)
+#else
+#define lockdep_assert_not_held(l)		do { (void)(l); } while (0)
+#endif /* CONFIG_LOCKDEP */
+#endif /* lockdep_assert_not_held */
+
 #endif /* __BACKPORT_LINUX_LOCKDEP_H */

commit c9d52475239e35cce287c1cdccd50c450ad66148

headers: fix lockdep_assert_not_held()
LOCK_STATE_HELD define was omitted during backport of
lockdep_assert_not_held() which leads to build failures of kernels with
CONFIG_LOCKDEP=y:

 backports-5.15.8-1/backport-include/linux/lockdep.h:16:47: error: 'LOCK_STATE_HELD' undeclared (first use in this function)

Fix it by adding missing LOCK_STATE_HELD define.

References: https://github.com/openwrt/openwrt/pull/9373
Fixes: af58b27b1b1a ("headers: Add lockdep_assert_not_held()")
Reported-by: Oskari Rauta <oskari.rauta@gmail.com>
Signed-off-by: Petr Å tetiar <ynezz@true.cz>

backport/backport-include/linux/lockdep.h
@@ -11,6 +11,9 @@ struct lockdep_map { };
 
 #ifndef lockdep_assert_not_held
 #ifdef CONFIG_LOCKDEP
+#ifndef LOCK_STATE_HELD
+#define LOCK_STATE_HELD		1
+#endif /* LOCK_STATE_HELD */
 #define lockdep_assert_not_held(l)	do {				\
 		WARN_ON(debug_locks &&					\
 			lockdep_is_held(l) == LOCK_STATE_HELD);		\