28c87bce2d9bf1fc9f68854bbf9b9f1d9ff9f851
Johannes Berg <johannes.berg@intel.com>
2022-10-11 23:04:29 +0200

    backports: add eth_hw_addr_set()
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    [Fix kernel condition]
    Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

---
+++ backport/backport-include/linux/etherdevice.h
@@ -0,0 +1,24 @@
+#ifndef _BACKPORT_LINUX_ETHERDEVICE_H
+#define _BACKPORT_LINUX_ETHERDEVICE_H
+#include_next <linux/etherdevice.h>
+#include <linux/version.h>
+
+/* This was backported to 4.19.291, but we do not support such high minor numbers use 255 instead. */
+#if LINUX_VERSION_IS_LESS(5,15,0) &&			\
+	!LINUX_VERSION_IN_RANGE(5,10,188, 5,11,0) &&	\
+	!LINUX_VERSION_IN_RANGE(5,4,251, 5,5,0) &&	\
+	!LINUX_VERSION_IN_RANGE(4,19,255, 4,20,0)
+/**
+ * eth_hw_addr_set - Assign Ethernet address to a net_device
+ * @dev: pointer to net_device structure
+ * @addr: address to assign
+ *
+ * Assign given address to the net_device, addr_assign_type is not changed.
+ */
+static inline void eth_hw_addr_set(struct net_device *dev, const u8 *addr)
+{
+	ether_addr_copy(dev->dev_addr, addr);
+}
+#endif /* LINUX_VERSION_IS_LESS(5,15,0) */
+
+#endif /* _BACKPORT_LINUX_ETHERDEVICE_H */
