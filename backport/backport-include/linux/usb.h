c6e5c4e9f22d3129ed22e1ab8e932fdd0a3c2c0d
Felix Fietkau <nbd@nbd.name>
2022-10-11 13:43:32 +0200

    headers: backport usb_maxpacket api change
    The third argument was removed
    
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/usb.h
+++ backport/backport-include/linux/usb.h
@@ -9,4 +9,12 @@ bool usb_check_bulk_endpoints(
 		const struct usb_interface *intf, const u8 *ep_addrs);
 #endif /* < 5.15 */
 
+#if LINUX_VERSION_IS_LESS(5,19,0)
+static inline u16 backport_usb_maxpacket(struct usb_device *udev, int pipe)
+{
+	return usb_maxpacket(udev, pipe, usb_pipeout(pipe));
+}
+#define usb_maxpacket LINUX_BACKPORT(usb_maxpacket)
+#endif /* <5.19 */
+
 #endif /* __BACKPORT_LINUX_USB_H */
