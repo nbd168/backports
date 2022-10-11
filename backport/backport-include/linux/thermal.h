7ef2ddfd37e350b1a18e47423bb295b0ae53dfed
Johannes Berg <johannes.berg@intel.com>
2022-10-11 23:04:45 +0200

    backports: add thermal_zone_device_enable()
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- backport/backport-include/linux/thermal.h
+++ backport/backport-include/linux/thermal.h
@@ -3,6 +3,18 @@
 #include_next <linux/thermal.h>
 #include <linux/version.h>
 
+#ifdef CONFIG_THERMAL
+#if LINUX_VERSION_IS_LESS(5,9,0)
+static inline int thermal_zone_device_enable(struct thermal_zone_device *tz)
+{ return 0; }
+#endif /* < 5.9.0 */
+#else /* CONFIG_THERMAL */
+#if LINUX_VERSION_IS_LESS(5,9,0)
+static inline int thermal_zone_device_enable(struct thermal_zone_device *tz)
+{ return -ENODEV; }
+#endif /* < 5.9.0 */
+#endif /* CONFIG_THERMAL */
+
 #if LINUX_VERSION_IS_LESS(5,9,0)
 #define thermal_zone_device_enable LINUX_BACKPORT(thermal_zone_device_enable)
 static inline int thermal_zone_device_enable(struct thermal_zone_device *tz)
