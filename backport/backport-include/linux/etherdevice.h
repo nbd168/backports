bfffbf65a052e0a43ecae10799134814996ae6e4
Johannes Berg <johannes.berg@intel.com>
2022-10-11 23:04:29 +0200

    backports: add eth_hw_addr_set()
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>

--- backport/backport-include/linux/etherdevice.h
+++ backport/backport-include/linux/etherdevice.h
@@ -38,4 +38,18 @@ static inline void u64_to_ether_addr(u64 u, u8 *addr)
 }
 #endif /* LINUX_VERSION_IS_LESS(4,11,0) */
 
+#if LINUX_VERSION_IS_LESS(5,15,0)
+/**
+ * eth_hw_addr_set - Assign Ethernet address to a net_device
+ * @dev: pointer to net_device structure
+ * @addr: address to assign
+ *
+ * Assign given address to the net_device, addr_assign_type is not changed.
+ */
+static inline void eth_hw_addr_set(struct net_device *dev, const u8 *addr)
+{
+	ether_addr_copy(dev->dev_addr, addr);
+}
+#endif /* LINUX_VERSION_IS_LESS(5,15,0) */
+
 #endif /* _BACKPORT_LINUX_ETHERDEVICE_H */
