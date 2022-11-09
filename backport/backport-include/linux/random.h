f4ec3262520e9c779c0bf965f8716e833a510ed8
Felix Fietkau <nbd@nbd.name>
2022-11-09 21:18:41 +0100

    headers: backport get_random_u16, get_random_u8
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/random.h
+++ backport/backport-include/linux/random.h
@@ -11,4 +11,16 @@ static inline u32 get_random_u32(void)
 }
 #endif
 
+#if LINUX_VERSION_IS_LESS(6,1,0)
+static inline u8 get_random_u8(void)
+{
+	return get_random_u32() & 0xff;
+}
+
+static inline u16 get_random_u16(void)
+{
+	return get_random_u32() & 0xffff;
+}
+#endif
+
 #endif /* __BACKPORT_RANDOM_H */
