b9d78695eef046b020dd27fe24a3b861c2350a09
Felix Fietkau <nbd@nbd.name>
2023-08-30 13:11:08 +0200

    headers: backport thermal_zone_device_priv
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/thermal.h
+++ backport/backport-include/linux/thermal.h
@@ -25,4 +25,16 @@ static inline int thermal_zone_device_disable(struct thermal_zone_device *tz)
 { return 0; }
 #endif /* < 5.9 */
 
+#if LINUX_VERSION_IS_LESS(6,4,0)
+#define thermal_zone_device_priv LINUX_BACKPORT(thermal_zone_device_priv)
+static inline void *thermal_zone_device_priv(struct thermal_zone_device *tzd)
+{
+#ifdef CONFIG_THERMAL
+	return tzd->devdata;
+#else
+	return NULL;
+#endif
+}
+#endif
+
 #endif /* __BACKPORT_LINUX_THERMAL_H */
