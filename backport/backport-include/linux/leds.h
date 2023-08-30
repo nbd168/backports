13005451c64994f958c7f4f92422d374ff885047
Felix Fietkau <nbd@nbd.name>
2023-08-30 12:08:01 +0200

    headers: backport led_trigger_blink/oneshot parameter changes
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

---
+++ backport/backport-include/linux/leds.h
@@ -0,0 +1,28 @@
+#ifndef __BACKPORT_LINUX_LEDS_H
+#define __BACKPORT_LINUX_LEDS_H
+#include_next <linux/leds.h>
+#include <linux/version.h>
+
+#if LINUX_VERSION_IS_LESS(6,5,0)
+static inline void
+LINUX_BACKPORT(led_trigger_blink_oneshot)(struct led_trigger *trigger,
+					  unsigned long delay_on,
+					  unsigned long delay_off,
+					  int invert)
+{
+	led_trigger_blink_oneshot(trigger, &delay_on, &delay_off, invert);
+}
+#define led_trigger_blink_oneshot LINUX_BACKPORT(led_trigger_blink_oneshot)
+
+static inline void
+LINUX_BACKPORT(led_trigger_blink)(struct led_trigger *trigger,
+				  unsigned long delay_on,
+				  unsigned long delay_off)
+{
+	led_trigger_blink(trigger, &delay_on, &delay_off);
+}
+#define led_trigger_blink LINUX_BACKPORT(led_trigger_blink)
+
+#endif /* < 6.5 */
+
+#endif /* __BACKPORT_LINUX_LEDS_H */
