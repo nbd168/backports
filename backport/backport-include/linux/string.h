302b7a46f7d194100b80eb9b7a7f9f6627da8710
Felix Fietkau <nbd@nbd.name>
2024-09-24 14:24:39 +0200

    headers: backport 2-arg mode of strscpy/strscpy_pad
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- backport/backport-include/linux/string.h
+++ backport/backport-include/linux/string.h
@@ -1,6 +1,7 @@
 #ifndef __BACKPORT_LINUX_STRING_H
 #define __BACKPORT_LINUX_STRING_H
 #include_next <linux/string.h>
+#include <linux/args.h>
 
 #ifndef memset_after
 #define memset_after(obj, v, member)					\
@@ -31,5 +32,22 @@
 		__p;							\
 	})
 #endif
+
+#if LINUX_VERSION_IS_LESS(6,9,0)
+#undef strscpy
+#define __strscpy0(dst, src, ...)	\
+	strscpy(dst, src, sizeof(dst) + __must_be_array(dst))
+#define __strscpy1(dst, src, size)	strscpy(dst, src, size)
+#define strscpy(dst, src, ...) \
+	CONCATENATE(__strscpy, COUNT_ARGS(__VA_ARGS__))(dst, src, __VA_ARGS__)
+
+#undef strscpy_pad
+#define __strscpy_pad0(dst, src, ...)	\
+	strscpy_pad(dst, src, sizeof(dst) + __must_be_array(dst))
+#define __strscpy_pad1(dst, src, size)	strscpy_pad(dst, src, size)
+#define strscpy_pad(dst, src, ...)	\
+	CONCATENATE(__strscpy_pad, COUNT_ARGS(__VA_ARGS__))(dst, src, __VA_ARGS__)
+
+#endif /* <6.9 */
 
 #endif /* __BACKPORT_LINUX_STRING_H */
